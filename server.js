d81426b167c15fb62992d8730b826b8c3d0932b4
yuziying25 <purpleguppytw2014@gmail.com>
2025-12-06 22:20:26 +0800

    調整部分資料成臺南市

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 /**
- * 取得高雄天氣預報
+ * 取得臺南天氣預報
  * CWA 氣象資料開放平臺 API
  * 使用「一般天氣預報-今明 36 小時天氣預報」資料集
  */
@@ -37,18 +37,18 @@ const getKaohsiungWeather = async (req, res) => {
       {
         params: {
           Authorization: CWA_API_KEY,
-          locationName: "宜蘭縣",
+          locationName: "臺南市",
         },
       }
     );
 
-    // 取得高雄市的天氣資料
+    // 取得臺南市的天氣資料
     const locationData = response.data.records.location[0];
 
     if (!locationData) {
       return res.status(404).json({
         error: "查無資料",
-        message: "無法取得高雄市天氣資料",
+        message: "無法取得臺南市天氣資料",
       });
     }
 
