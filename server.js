e4e293625524763c2ded711bea085d71fedac253
yuziying25 <purpleguppytw2014@gmail.com>
2025-12-06 22:42:08 +0800

    再次調整API連結成臺南

--- server.js
+++ server.js
@@ -131,7 +131,7 @@ app.get("/", (req, res) => {
   res.json({
     message: "歡迎使用 CWA 天氣預報 API",
     endpoints: {
-      kaohsiung: "/api/weather/kaohsiung",
+      Tainan: "/api/weather/Tainan",
       health: "/api/health",
     },
   });
